f517938773965d65da5a68816d365b725d3a47ca
AngeloAllanic <angelo.allanic130459@my.lsgh.edu.ph>
2021-04-21 20:37:30 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ionic-co6xvw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ionic-co6xvw)
---
+++ pages/home/home.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  op: number;
+  sp: number;
+ 
+  s: number;
+  mr: number;
+
+  constructor(public navCtrl: NavController) {}
+
+  calculateMCA() {
+    this.s = (this.op - this.sp);
+    this.s = parseFloat(this.s.toFixed(2));
+
+    this.mr = (this.s/this.sp) * 100; 
+    this.mr = parseFloat(this.mr.toFixed(2));
+  }
+  
+}
